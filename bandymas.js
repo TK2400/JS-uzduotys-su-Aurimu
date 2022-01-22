aca6ff8b0ebed46757dc8a7b9c3cf7ec845eec39
T.Kav <tomas@plienas.lt>
2022-01-22 14:36:06 +0200

    uzduotis su farenheito formule

--- bandymas.js
+++ bandymas.js
@@ -1,16 +1,20 @@
-// #3 papildyti koda:
-//  - funkcija priima viena paramtera "name"
-//  - funcija turi tik grazinti pasisveikinima sujungta
-//     su duotu vardu
-//  - pakviesti funkcija viena karta su savo vardu
-//    - is funkcijos kvietimo gauta verte alertinti
+// function dviejuSkaiciuSuma(a, b) {
+//     return a + b
+// }
 
+// const atsakymas = dviejuSkaiciuSuma(15, 16)
+// alert(atsakymas)
 
-
-function sayHi(name) {
-    return 'laba diena' + "," + " " + name
-
+function farenheitoFormule(a) {
+    return a * 1.8 + 32
 }
 
-const atsakymas = sayHi('Tomas')
-alert(atsakymas)
+const ats1 = farenheitoFormule(-5)
+const ats2 = farenheitoFormule(0)
+const ats3 = farenheitoFormule(5)
+const ats4 = farenheitoFormule(10)
+
+console.log(ats1)
+console.log(ats2)
+console.log(ats3)
+console.log(ats4)
