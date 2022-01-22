b9172d3d0b5e03eff2c93c8f88ea3b71ab8c501f
T.Kav <tomas@plienas.lt>
2022-01-22 10:46:35 +0200

    saskaitos vidurys

--- bandymas.js
+++ bandymas.js
@@ -1,70 +1,7 @@
-// deklaruoti biudzeta
+alert('cia mes kvieciam fnckija');
 
-//deklaruoti kaina/
-
-//jeigu turi pakankamai, pasakayti, kad ok, galiu pirkti
-// hello git hub
-
-
-
-const myBudget = Number(prompt("koks biudzetas"))
-console.log(myBudget)
-const prekesKaina = 103
-    // NaN - ne skaicius
-
-
-
-if (myBudget > prekesKaina) {
-    alert("valio, galiu pirkti")
-} else if (myBudget === prekesKaina) {
-    alert("kaip tycia uztenka lygiai")
-} else {
-    alert("reikia dar pataupyti")
+function sunkusSestadienis(parametras) {
+    return parametras + 1;
 }
-
-console.log("programa baigta")
-
-
-
-// const vardas = prompt("labas, koks tavo vardas") // browseris klausia ir ledizai irasyti
-// console.log(typeof vardas)
-// prompt visada grazina string 
-
-
-
-
-
-// const car = "Audi"
-
-
-// if(car === "BMW"||car === "Mini"||car === "Rolls-Royce"){
-//   alert("priklauso BMW grupei")
-// } else if (car === "VW"||car === "Audi" || car === "Bentley" ||car === "Bugatti" ||car === "Lamborghini" || car ==="Porsche" ){
-//   alert("priklauso VW grupei")
-// } else {
-//     alert ("yra no name brandas")
-// }
-
-// const legalAge = 20
-// const clientAge =21
-// if (legalAge < clientAge) {
-//     alert("klientas per jaunas")
-// } else alert("prasome pasirinkti preke")
-
-// const manoVardas = "12345678910"
-// if (manoVardas.length < 6 ) {
-//     alert ("trumpas vardas")
-// } else if (manoVardas.length >= 6 && manoVardas.length <= 10) {
-//     alert ("ilgas vardas")
-// } else if (manoVardas.length > 10) {
-//     alert ("labai ilgas vardas")
-// }
-
-// const myAge = 47
-// if (myAge >= 100 || myAge <= 0) {
-//     alert ("invalid age")
-// } else if (myAge >= 0.1 && myAge < 18) {
-//     alert ("child")
-// } else if (myAge >= 18 && myAge <= 99) {
-//     alert("Adult")
-// }
+const kibiras = sunkusSestadienis(6);
+console.log(kibiras)
