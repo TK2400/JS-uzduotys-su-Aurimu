34c7826ae43cc5957339136f74af38a95dc2d98a
T.Kav <tomas@plienas.lt>
2022-02-19 13:55:04 +0200

    uzduotis su saraso padarymu salia radio itemsu

--- 43-ios paskaitos uzduotys DOM/43.1.js
+++ 43-ios paskaitos uzduotys DOM/43.1.js
@@ -1,8 +1,9 @@
 const phones = [
-    'aphone',
-    'bphone',
-    'cphone',
-
+    'samsung',
+    'nokia',
+    'iphone',
+    'sony',
+    'xphone',
 ]
 
 /*createInput(phones[0])
@@ -10,24 +11,47 @@ createInput(phones[1])
 createInput(phones[2])*/ // tai butu analogiskas uzrasymas forEach
 
 
-phones.forEach(createInput)
+phones.forEach(createInput);
+phones.forEach(list)
+phones.filter(showOnly)
+
 
 function createInput(telefonas) {
-    console.log(telefonas)
-
     const input = document.createElement('input')
     const label = document.createElement('label')
     input.setAttribute('name', 'phone')
     input.setAttribute('value', telefonas)
     input.setAttribute('type', 'radio')
-
     label.innerHTML = telefonas
     label.appendChild(input)
-
-    console.log(label)
     const form = document.querySelector('form')
     form.appendChild(label)
+}
 
 
+function list(telefonas) {
+    const p = document.createElement('p')
+    p.innerHTML = telefonas
+    const div = document.querySelector('div')
+    div.appendChild(p)
 
+}
+
+
+// Aurimo sprendimas
+
+function createList(telefonas) {
+    const ul = document.querySelector('ul')
+    const li = document.querySelector('li')
+    li.innerHTML = telefonas;
+    ul.appendChild(li)
+}
+
+
+function showOnly(telefonas) {
+    //     const p = document.createElement('p')
+    //     p.innerHTML = telefonas
+    //     const div = document.querySelector('div')
+    //     div.appendChild(p)
+    // 
 }
