a7022d8c364735a1cedde030f06bfccf36a4a14a
T.Kav <tomas@plienas.lt>
2022-02-19 16:56:46 +0200

    tik su destytojo pagaba atlikta uzduotis su is saraso isfiltruojamu telefonu, atlikta, logika nera visai aiski

--- 43-ios paskaitos uzduotys DOM/43.1.js
+++ 43-ios paskaitos uzduotys DOM/43.1.js
@@ -1,57 +1,48 @@
+const ul = document.querySelector('ul')
+let pasirinkimas = ''
 const phones = [
     'samsung',
     'nokia',
-    'iphone',
-    'sony',
-    'xphone',
+    'iphone'
 ]
 
-/*createInput(phones[0])
-createInput(phones[1])
-createInput(phones[2])*/ // tai butu analogiskas uzrasymas forEach
-
-
 phones.forEach(createInput);
-phones.forEach(list)
-phones.filter(showOnly)
+renderList(phones)
 
+function renderList(x) {
+    ul.innerHTML = '';
+    x.forEach(createListItem);
+}
 
 function createInput(telefonas) {
     const input = document.createElement('input')
     const label = document.createElement('label')
+
+    input.addEventListener('change', filterCatalogue)
     input.setAttribute('name', 'phone')
     input.setAttribute('value', telefonas)
     input.setAttribute('type', 'radio')
+
     label.innerHTML = telefonas
     label.appendChild(input)
+
     const form = document.querySelector('form')
     form.appendChild(label)
 }
 
-
-function list(telefonas) {
-    const p = document.createElement('p')
-    p.innerHTML = telefonas
-    const div = document.querySelector('div')
-    div.appendChild(p)
-
-}
-
-
-// Aurimo sprendimas
-
-function createList(telefonas) {
-    const ul = document.querySelector('ul')
-    const li = document.querySelector('li')
+function createListItem(telefonas) {
+    const li = document.createElement('li')
     li.innerHTML = telefonas;
+
     ul.appendChild(li)
 }
 
+function filterCatalogue(event) {
+    pasirinkimas = event.target.value
+    const filteredPhones = phones.filter(phoneFilter)
+    renderList(filteredPhones)
+}
 
-function showOnly(telefonas) {
-    //     const p = document.createElement('p')
-    //     p.innerHTML = telefonas
-    //     const div = document.querySelector('div')
-    //     div.appendChild(p)
-    // 
+function phoneFilter(verte) {
+    return verte === pasirinkimas
 }
